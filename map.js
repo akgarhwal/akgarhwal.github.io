d12feaa5488f1863fb9b1ff7d9fda8c1c8fdc8a4
Abhinesh Garhwal <abhinesh.garhwal@gmail.com>
2018-08-21 10:35:04 +0530

    Update map.js

--- map.js
+++ map.js
@@ -3,6 +3,29 @@ var weatherKey = "b0e74e60f082fb0478861b4892261ae0";
 var infowindow;
 // Main function to init map object and load current location of user
 function makeMap() {
+    // $.ajax({
+    //     url: "http://ip-api.com/json", success: function (result) {
+    //         var myLocation = { lat: result.lat, lng: result.lon };
+    //         var mapElement = document.getElementById('map');
+    //         $("#map").height($(window).height() - 20);
+    //         var map = new google.maps.Map(mapElement, { zoom: 5, center:  myLocation });
+    //         getWeather( myLocation, map,  myLocation.lat,  myLocation.lng);
+    //         google.maps.event.addListener(map, 'click', function (event) {
+    //             let lat = event.latLng.lat();
+    //             let lng = event.latLng.lng();
+    //             getWeather(event.latLng, map, lat, lng);
+
+    //         });
+    //     }
+    // });
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+
+    }
+
+}
+function showError(error) {
     $.ajax({
         url: "http://ip-api.com/json", success: function (result) {
             var myLocation = { lat: result.lat, lng: result.lon };
@@ -17,6 +40,23 @@ function makeMap() {
 
             });
         }
+    });
+}
+function showPosition(position) {
+    alert('h');
+    //x.innerHTML="Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;  
+    //getWeather(position, position.coords.latitude, position.coords.longitude);
+    //alert(position);
+    var myLocation = { lat: position.coords.latitude, lng: position.coords.longitude };
+    var mapElement = document.getElementById('map');
+    $("#map").height($(window).height() - 20);
+    var map = new google.maps.Map(mapElement, { zoom: 5, center:  myLocation });
+    getWeather( myLocation, map,  myLocation.lat,  myLocation.lng);
+    google.maps.event.addListener(map, 'click', function (event) {
+        let lat = event.latLng.lat();
+        let lng = event.latLng.lng();
+        getWeather(event.latLng, map, lat, lng);
+
     });
 }
 // function to get weather data
